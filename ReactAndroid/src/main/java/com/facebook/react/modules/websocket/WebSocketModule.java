2fb0bcf8712ccc55ea9a651c6a15c628e0f51f75
Jace <Jace-Samsung@users.noreply.github.com>
2023-02-13 17:48:41 -0600

    Update WebSocketModule.java
    Minimal change alongside post-install script to build custom HostnameVerifier.

--- ReactAndroid/src/main/java/com/facebook/react/modules/websocket/WebSocketModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/websocket/WebSocketModule.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import javax.net.ssl.HostnameVerifier;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
